571d9de36e17f5d00c923dedb1e4a086bf0105da
정승현 <107786773+SeungHyun29@users.noreply.github.com>
2022-10-25 16:19:43 +0900

    [FEAT] CreateLesson API 생성(#61)

--- src/apis/images/image.service.ts
+++ src/apis/images/image.service.ts
@@ -1,8 +1,29 @@
 import { Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
+import { Image } from './entities/image.entity';
 
 @Injectable()
 export class ImagesService {
-  constructor() {
-    //
+  constructor(
+    @InjectRepository(Image)
+    private readonly imagesRepository: Repository<Image>, //
+  ) {}
+
+  async createLessonImage({ image, result }) {
+    return await Promise.all(
+      image.map(
+        (el, idx) =>
+          new Promise((resolve, reject) => {
+            this.imagesRepository.save({
+              isMain: idx === 0 ? true : false,
+              url: el,
+              lesson: { id: result.id },
+            });
+            resolve('이미지 저장 완료');
+            reject('이미지 저장 실패');
+          }),
+      ),
+    );
   }
 }
--- src/apis/lessons/lessons.service.ts
+++ src/apis/lessons/lessons.service.ts
@@ -1,8 +1,42 @@
 import { Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
+import { Image } from '../images/entities/image.entity';
+import { ImagesService } from '../images/image.service';
+import { User } from '../users/entities/user.entity';
+import { Lesson } from './entites/lesson.entity';
 
 @Injectable()
 export class LessonsService {
-  constructor() {
-    //
+  constructor(
+    @InjectRepository(Lesson)
+    private readonly lessonsRepository: Repository<Lesson>,
+
+    @InjectRepository(Image)
+    private readonly imagesRepository: Repository<Image>,
+
+    @InjectRepository(User)
+    private readonly usersRepository: Repository<User>,
+
+    private readonly imagesService: ImagesService,
+  ) {}
+
+  async create({ user, createLessonInput }) {
+    const { location, image, ...lesson } = createLessonInput;
+
+    const _user = await this.usersRepository.findOne({
+      where: { email: user },
+    });
+
+    const result = await this.lessonsRepository.save({
+      ...lesson,
+      location,
+      image,
+      user: _user.id,
+    });
+
+    if (image) {
+      await this.imagesService.createLessonImage({ image, result });
+    }
   }
 }
