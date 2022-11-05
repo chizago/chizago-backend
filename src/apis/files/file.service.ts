47e35d7f5950cb0c9a8c37753f0d7c4e2e141486
정승현 <107786773+SeungHyun29@users.noreply.github.com>
2022-11-05 19:04:49 +0900

    [MODIFY] uploadFile 수정(#117)

--- src/apis/files/file.service.ts
+++ src/apis/files/file.service.ts
@@ -4,26 +4,26 @@ import { v4 as uuidv4 } from 'uuid';
 
 @Injectable()
 export class FileService {
-  // async upload({ files }) {
-  //   const waitedFiles = await Promise.all(files);
-  //   const bucket = 'team04-storage';
-  //   const storage = new Storage({
-  //     projectId: process.env.PROJECT_ID,
-  //     keyFilename: process.env.BUCKET,
-  //   }).bucket(bucket);
-  //   const results = await Promise.all(
-  //     waitedFiles.map(
-  //       (file) =>
-  //         new Promise((resolve, reject) => {
-  //           const fname = `${uuidv4()}/origin/${file.filename}`;
-  //           file
-  //             .createReadStream()
-  //             .pipe(storage.file(fname).createWriteStream())
-  //             .on('finish', () => resolve(`${fname}`))
-  //             .on('error', () => reject('실패'));
-  //         }),
-  //     ),
-  //   );
-  //   return results;
-  // }
+  async upload({ files }) {
+    const waitedFiles = await Promise.all(files);
+    const bucket = 'team04-storage';
+    const storage = new Storage({
+      projectId: process.env.PROJECT_ID,
+      keyFilename: process.env.BUCKET,
+    }).bucket(bucket);
+    const results = await Promise.all(
+      waitedFiles.map(
+        (file) =>
+          new Promise((resolve, reject) => {
+            const fname = `${uuidv4()}/origin/${file.filename}`;
+            file
+              .createReadStream()
+              .pipe(storage.file(fname).createWriteStream())
+              .on('finish', () => resolve(`${fname}`))
+              .on('error', () => reject('실패'));
+          }),
+      ),
+    );
+    return results;
+  }
 }
