8dec2f4a8ceeb42b3f659cd60b3b0cbbd7be270b
1hana26 <seolhana6206@gmail.com>
2022-11-14 17:40:24 +0900

    [FEAT]createReview API 생성(#126)

--- src/apis/classReviews/classReview.service.ts
+++ src/apis/classReviews/classReview.service.ts
@@ -1,12 +1,56 @@
-import { Injectable } from '@nestjs/common';
+import { ConflictException, Injectable } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Repository } from 'typeorm';
+import { ClassApplicant } from '../classApplicants/entities/classApplicant.entity';
+import { Lesson } from '../lessons/entites/lesson.entity';
+import { User } from '../users/entities/user.entity';
 import { ClassReview } from './entities/classReview.entity';
 
 @Injectable()
 export class ClassReviewService {
   constructor(
     @InjectRepository(ClassReview)
-    private readonly userRepository: Repository<ClassReview>,
+    private readonly classReview: Repository<ClassReview>,
+    @InjectRepository(User)
+    private readonly userRepository: Repository<User>,
+    @InjectRepository(Lesson)
+    private readonly lessonRepository: Repository<Lesson>,
+    @InjectRepository(ClassApplicant)
+    private readonly classApplicant: Repository<ClassApplicant>,
   ) {}
+
+  async create({ id, contents, email }) {
+    //사용자 정보 찾기
+    const user: any = await this.userRepository.findOne({
+      where: { email },
+    });
+
+    //신청자에 유저 존재 찾기
+    const classApplicantUSer: any = await this.classApplicant.findOne({
+      where: {
+        user: { id: user.id },
+      },
+      relations: ['user', 'lesson'],
+    });
+    //신청자 정보 예외처리
+    if (!classApplicantUSer) {
+      throw new ConflictException('신청 정보가 없습니다.');
+    }
+
+    //lesson 찾기
+    const lesson: any = await this.lessonRepository.find({
+      where: { id },
+    });
+    //lesson 예외처리
+    if (!lesson) {
+      throw new ConflictException('해당 수업 정보가 없습니다.');
+    }
+
+    //리뷰 저장하기
+    return this.classReview.save({
+      contents,
+      user,
+      lesson,
+    });
+  }
 }
