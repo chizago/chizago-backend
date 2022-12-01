8548fcafd20b52f796b696235a8a977dc4578e3d
1hana26 <seolhana6206@gmail.com>
2022-12-01 18:23:06 +0900

    [FEAT] createReport API 추가(#153)

--- src/apis/reports/reports.resolver.ts
+++ src/apis/reports/reports.resolver.ts
@@ -1,4 +1,6 @@
-import { Query, Resolver } from '@nestjs/graphql';
+import { UseGuards } from '@nestjs/common';
+import { Args, Context, Mutation, Query, Resolver } from '@nestjs/graphql';
+import { GqlAuthAccessGuard } from 'src/commons/auth/gql-auth.guard';
 import { Report } from './entities/report.entity';
 import { ReportsService } from './reports.service';
 
@@ -13,5 +15,17 @@ export class ReportsResolver {
   @Query(() => [Report])
   fetchReports() {
     return this.reportsService.find();
+  }
+
+  @UseGuards(GqlAuthAccessGuard)
+  @Mutation(() => Report)
+  createReports(
+    @Context() context: any, //
+    @Args({ name: 'type', description: '신고 타입' }) type: string,
+    @Args({ name: 'contents', description: 'etc일때 적는 내용' })
+    contents: string,
+  ) {
+    const email = context.req.user.email;
+    return this.reportsService.create({ email, type, contents });
   }
 }
